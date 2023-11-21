cb1b73d9e66c7350241799131e101d4ced9863d9
888Greys <toxicgreys001@gmail.com>
2023-11-21 07:49:40 -0800

    Sorting algorithms

--- deck.h
+++ deck.h
@@ -5,10 +5,10 @@
 
 /**
 * enum kind_e - Enumeration of card suits.
-* @SPADE: Represents the Spades suit.
-* @HEART: Represents the Hearts suit.
-* @CLUB: Represents the Clubs suit.
-* @DIAMOND: Represents the Diamonds suit.
+* @SPADE: Spades suit.
+* @HEART: Hearts suit.
+* @CLUB: Clubs suit.
+* @DIAMOND: Diamonds suit.
 */
 typedef enum kind_e
 {
@@ -19,13 +19,11 @@ DIAMOND
 } kind_t;
 
 /**
-* struct card_s - Structure representing
-*a playing card.
+* struct card_s - Playing card
 *
-* @value: The value of the card, ranging
-*from "Ace" to "King".
-* @kind: The suit of the card
-*(SPADE, HEART, CLUB, DIAMOND).
+* @value: Value of the card
+* From "Ace" to "King"
+* @kind: Kind of the card
 */
 typedef struct card_s
 {
@@ -34,12 +32,11 @@ const kind_t kind;
 } card_t;
 
 /**
-* struct deck_node_s - Structure representing
-*a node in a deck of cards.
+* struct deck_node_s - Deck of card
 *
-* @card: Pointer to the card associated with the node.
-* @prev: Pointer to the previous node in the deck.
-* @next: Pointer to the next node in the deck.
+* @card: Pointer to the card of the node
+* @prev: Pointer to the previous node of the list
+* @next: Pointer to the next node of the list
 */
 typedef struct deck_node_s
 {
@@ -48,12 +45,6 @@ struct deck_node_s *prev;
 struct deck_node_s *next;
 } deck_node_t;
 
-/**
-* sort_deck - Sorts a deck of cards
-*in ascending order.
-*
-* @deck: Pointer to the head of the deck.
-*/
 void sort_deck(deck_node_t **deck);
 
 #endif /* DECK_H */
--- sort.h
+++ sort.h
@@ -10,8 +10,8 @@
 
 /**
 * enum bool - Enumeration of Boolean values.
-* @false: Represents the value 0.
-* @true: Represents the value 1.
+* @false: Equals 0.
+* @true: Equals 1.
 */
 typedef enum bool
 {
@@ -20,11 +20,11 @@ true
 } bool;
 
 /**
-* struct listint_s - Doubly linked list node.
+* struct listint_s - Doubly linked list node
 *
-* @n: Integer stored in the node.
-* @prev: Pointer to the previous element of the list.
-* @next: Pointer to the next element of the list.
+* @n: Integer stored in the node
+* @prev: Pointer to the previous element of the list
+* @next: Pointer to the next element of the list
 */
 typedef struct listint_s
 {
@@ -37,7 +37,7 @@ struct listint_s *next;
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/* Sorting algorithms */
+/* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -49,14 +49,6 @@ void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
-
-/**
-* quick_sort_hoare - Sorts an array of integers
-*using the Quick Sort algorithm (Hoare partition scheme).
-*
-* @array: Array of integers to be sorted.
-* @size: Size of the array.
-*/
 void quick_sort_hoare(int *array, size_t size);
 
 #endif /* SORT_H */
